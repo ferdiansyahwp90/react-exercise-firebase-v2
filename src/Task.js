2b5c7a6dcb1449da6be8f316422097b0d45a4690
ferdiansyahwp90 <wardanaputraferdiansyah@gmail.com>
2023-06-20 11:53:57 +0700

    add

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://fp-fwp.web.app/"; // TODO: replace this
+const studentName = "Ferdiansyah Wardana Putra"; // TODO: replace this
+const studentId = "FE5094717"; // TODO: replace this
+
+export { url, studentName, studentId };
